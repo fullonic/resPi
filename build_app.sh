fa245550f1d77994fd9fc1f0a3b8248cc181fb83
fullonic <somnium@riseup.net>
2020-01-13 19:02:02 +0100

    Update compile pyinstall bash script

--- build_app.sh
+++ build_app.sh
@@ -1,6 +1,7 @@
 echo Build app
 echo -------------------------------------
-pyinstaller --onedir --add-data 'templates:templates' --add-data 'static:static' --add-data 'logs:logs' --add-data "venv/lib/python3.8/site-packages/plotly:plotly" app.py
+pyinstaller --onedir --icon=icon.ico --add-data 'templates:templates' --add-data 'static:static' --add-data 'logs:logs' --add-data "config.json:." --add-data "ve
+nv/lib/python3.8/site-packages/plotly:plotly" app.py
 echo copying files
 echo -------------------------------------
 cp config.json dist/app/config.json
